3c41f5a1508b8ca7bc734e26906f39cdad688378
Bianca Navey <74916027+biancanavey@users.noreply.github.com>
2021-07-18 23:59:59 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+// Exercise 5.1: creating a countdown loop
+
+//var i = 0; 
+
+//for(i=10); i >0; i-- {
+//alert(i); 
+//}
+
+// Exercise 5.2 Create a function that uses a prompt 
+
+function myName(){
+    var askName= prompt('what is your name');
+    return askName;
+}
+
+var result = myName()
+console.log(result);
+
+function helloWorld() {
+    console.log ('hello world');
+
+}
+
+function sum(a, b) {
+    return a + b;
+  }
+  var myResult = sum(1,2);
+
+  function returnHello() {
+    return 'Hello World'
+  }
+  var greeting = returnHello();
+
+function sum (number 1, number 2) {}
